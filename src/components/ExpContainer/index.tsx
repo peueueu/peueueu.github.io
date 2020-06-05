73262cd5a245bdd08061eecaaddbeb8696335518
peueueu <silvasantospedro91@gmail.com>
2020-06-05 15:14:00 -0300

    Removing All Content

--- src/components/ExpContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ExpWrapper } from './styled';
-
-const ExpContainer: React.FC = (props) => {
-  return <ExpWrapper>{props.children}</ExpWrapper>
-}
-
-export default ExpContainer;
--- src/styles/colors.ts
+++
@@ -1,6 +0,0 @@
-export const oblivionTheme = {
-  darker: '#171E26',
-  details: '#F25252',
-  textColor: '#0f0f0f',
-  textDetails: '#F25041',
-}
