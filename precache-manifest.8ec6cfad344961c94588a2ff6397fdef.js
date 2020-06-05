025c5572b210c5d558f8426ccee9d52ebad589ba
peueueu <silvasantospedro91@gmail.com>
2020-06-05 15:35:03 -0300

    Feat: Deploy

---
+++ precache-manifest.8ec6cfad344961c94588a2ff6397fdef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572717b89f58d4a5dec6bdda102ec4f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2eebc8fcea5289b30e62",
+    "url": "./static/js/2.d21d3981.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d21d3981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9530678886a0d0209ee8",
+    "url": "./static/js/main.ace342d2.chunk.js"
+  },
+  {
+    "revision": "1dadb82055d25b5da444",
+    "url": "./static/js/runtime-main.8c907a67.js"
+  },
+  {
+    "revision": "0e6c6d4f34e4faf541a719921339e168",
+    "url": "./static/media/peueueu.0e6c6d4f.jpeg"
+  }
+]);
